0ee8fedd77b481308393efaaf1ca012eb4081bc1
Julián Gamboa <juliandg34@gmail.com>
2019-08-19 13:32:13 -0500

    components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import Services from '../components/Services';
+import { Link } from 'react-router-dom';
+import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
     return (
-        <div>
-            Hello from home page
-        </div>
+        <>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to='/rooms' className="btn-primary">
+                        Our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     )
 }
